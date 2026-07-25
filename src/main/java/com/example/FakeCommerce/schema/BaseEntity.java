bf6fa9a4b6595faa7dc13f69fea73d15e1a32de9
Roshan Prajapati <roshanprajapati2004@gmail.com>
2026-07-25 09:44:59 +0530

    Added created_At and updated_At

--- src/main/java/com/example/FakeCommerce/schema/BaseEntity.java
+++ src/main/java/com/example/FakeCommerce/schema/BaseEntity.java
@@ -1,5 +1,13 @@
 package com.example.FakeCommerce.schema;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,6 +39,8 @@ import lombok.Data;
  * Hibernate DOES NOT create a separate table named BaseEntity.
  */
 @MappedSuperclass
+
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     /*
@@ -58,4 +68,13 @@ public class BaseEntity {
      */
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/example/FakeCommerce/schema/Order.java
+++ src/main/java/com/example/FakeCommerce/schema/Order.java
@@ -1,11 +1,6 @@
 package com.example.FakeCommerce.schema;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,11 +16,4 @@ import lombok.NoArgsConstructor;
 public class Order extends BaseEntity {
 
     private OrderStatus status;
-
-    @ManyToMany
-    @JoinTable(name = "order_products", joinColumns = @JoinColumn(name = "order_id"), // the FK belonging to the same  schema -- Order
-            inverseJoinColumns = @JoinColumn(name = "product_id") // the FK belonging to the other schema -- Product
-    )
-    private List<Product> products;
-
 }
